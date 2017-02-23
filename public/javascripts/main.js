06bf164ae788f5b5a269efdf08732231282d2b32
Steven Julien <stevenjulien@mail.weber.edu>
2017-02-22 22:03:42 -0700

    usernames are written with chats

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -1,29 +1,34 @@
 $(document).ready(function(){
-    //let messages = [];
+    // let usersOnline = [];
     let socket = io.connect('http://localhost:3000');
     let chatForm = $('#chatForm');
     let message = $('#chatInput');
     let chatWindow = $('#chatWindow');
-    
-    let username = $('#username');
-    let users = $('#users');
+    let username = '';
+    //let users = $('#users');
     let error = $('#error');
+
+    socket.emit('getUser');
+
+    socket.on('user', data => {
+        username = data.username;
+        //users.append(data.username + '<br>');
+    });
 
     chatForm.on('submit', function(e){
          e.preventDefault();
-         // TODO: Send a chat object rather than a string
          socket.emit('c2smsg', message.val());
          message.val('');
     });
 
     socket.on('s2cmsg', function(data){
-        // TODO: Get the pieces of a chat object and display them instead of just a string
-        chatWindow.append('<strong>I haven\'t figured out how to get the user yet: </strong>'+ data + '<br>');
+        chatWindow.append('<strong>' + data.person + ': </strong>'+ data.message + '<br>');
     });
 
-    socket.on('userLoggedOut', () => {
-        // TODO: Get the username and insert it 
-        chatWindow.append('Somebody logged out');
+    socket.on('userLoggedOut', (data) => {
+        chatWindow.append('<strong>'+data+'</strong> left the chat room.<br>');
     })
+
+    
 
 });
--- routes/socket.js
+++ routes/socket.js
@@ -1,20 +1,28 @@
 module.exports = (io) => {
-    //io.on('connection', socket => {  // I added sockets as that is hwo i've seen examples, don't understand any differences
     io.sockets.on('connection', socket => {
         
-        // TODO: get the username, show name in users div on chatroom page
+        // TODO: show all current usernames in users div on chatroom page
+        
+        const user = { name: socket.request.user.name, username: socket.request.user.username };
+        
+        //console.log('req.user.name: ' + user.name);
+        //console.log('req.user.username: ' + user.username);
+
+        socket.on('getUser', (callback) => {
+            //console.log('got a getUser');
+            io.sockets.emit('user', user);
+        });
         
         // Client to Server message
         socket.on('c2smsg', function(data, callback){
-            // TODO: get the username, write the message to the database
-            //maybe help here https://www.npmjs.com/package/passport.socketio
-            io.sockets.emit('s2cmsg', data);
+            // TODO: write the message to the database
+            var chatObject = {person: user.username, message: data};
+            io.sockets.emit('s2cmsg', chatObject);
         });
 
         // Notify the chat room the user disconnected
         socket.on('disconnect', socket => {
-            // TODO: get the username, send it as a second parm
-            io.sockets.emit('userLoggedOut');
+            io.sockets.emit('userLoggedOut', user.username);
         });
 
     });// end on connection event
